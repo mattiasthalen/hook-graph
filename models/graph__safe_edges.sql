56f3083ccca04b387b3c5c50a693b356027125b8
Mattias Thalén <bitter-polders0x@icloud.com>
2025-09-18 09:52:23 +0000

    Refactor SQL view for safe edge

--- models/graph__safe_edges.sql
+++ models/graph__safe_edges.sql
@@ -1,83 +1,90 @@
 MODEL (
   name graph.safe_edges,
-  kind VIEW,
-  enabled TRUE
+  enabled TRUE,
+  kind VIEW
 );
 
-WITH cte__edges AS (
-  SELECT *
-  FROM graph.edges
-),
-
-cte__from_stats AS (
+WITH cte__nodes AS (
   SELECT
-      frame                       AS from_frame,
-      from_concept,
-      from_keyset,
-      from_hook_name,
-      to_concept,
-      to_keyset,
-      COUNT(*)                    AS from_count
+    *
+  FROM graph.nodes
+), cte__edges AS (
+  SELECT
+    *
+  FROM graph.edges
+), cte__from_stats AS (
+  SELECT
+    frame AS from_frame,
+    from_concept,
+    from_keyset,
+    from_hook_name,
+    to_concept,
+    to_keyset,
+    COUNT(*) AS from_count
   FROM cte__edges
   GROUP BY ALL
-),
-
-cte__to_stats AS (
+), cte__to_stats AS (
   SELECT
-      src.frame                   AS from_frame,
-      src.from_concept,
-      src.from_keyset,
-      src.from_hook_name,
-      dst.frame                   AS to_frame,
-      src.to_concept,
-      src.to_keyset,
-      dst.from_hook_name          AS to_hook_name,
-      COUNT(*)                    AS to_count
-  FROM cte__edges AS src
-  INNER JOIN cte__edges AS dst
-    ON  src.to_hook    = dst.from_hook
-    AND src.to_concept = dst.from_concept
-    AND src.to_keyset  = dst.from_keyset
+    cte__edges.frame AS from_frame,
+    cte__edges.from_concept,
+    cte__edges.from_keyset,
+    cte__edges.from_hook_name,
+    cte__nodes.frame AS to_frame,
+    cte__edges.to_concept,
+    cte__edges.to_keyset,
+    cte__nodes.hook_name AS to_hook_name,
+    COUNT(*) AS to_count
+  FROM cte__edges
+  INNER JOIN cte__nodes
+    ON cte__edges.to_concept = cte__nodes.concept
+    AND cte__edges.to_keyset = cte__nodes.keyset
+    AND cte__edges.to_hook = cte__nodes.hook
+    AND (
+      1 = 1
+      AND cte__edges.frame <> cte__nodes.frame
+      OR cte__edges.from_hook_name <> cte__nodes.hook_name
+    )
   GROUP BY ALL
-),
-
-cte__safe_sigs AS (
+), cte__safe_sigs AS (
   SELECT
-      fs.from_frame,
-      fs.from_concept,
-      fs.from_keyset,
-      fs.from_hook_name,
-      ts.to_frame,
-      fs.to_concept,
-      fs.to_keyset,
-      ts.to_hook_name
-  FROM cte__from_stats AS fs
-  INNER JOIN cte__to_stats AS ts
-    ON  ts.from_frame     = fs.from_frame
-    AND ts.from_concept   = fs.from_concept
-    AND ts.from_keyset    = fs.from_keyset
-    AND ts.from_hook_name = fs.from_hook_name
-    AND ts.to_concept     = fs.to_concept
-    AND ts.to_keyset      = fs.to_keyset
-  WHERE ts.to_count <= fs.from_count
+    cte__from_stats.from_frame,
+    cte__from_stats.from_concept,
+    cte__from_stats.from_keyset,
+    cte__from_stats.from_hook_name,
+    cte__to_stats.to_frame,
+    cte__from_stats.to_concept,
+    cte__from_stats.to_keyset,
+    cte__to_stats.to_hook_name,
+    cte__from_stats.from_count,
+    cte__to_stats.to_count,
+    cte__to_stats.to_count = cte__from_stats.from_count AS is_safe
+  FROM cte__from_stats
+  INNER JOIN cte__to_stats
+    ON cte__to_stats.from_frame = cte__from_stats.from_frame
+    AND cte__to_stats.from_concept = cte__from_stats.from_concept
+    AND cte__to_stats.from_keyset = cte__from_stats.from_keyset
+    AND cte__to_stats.from_hook_name = cte__from_stats.from_hook_name
+    AND cte__to_stats.to_concept = cte__from_stats.to_concept
+    AND cte__to_stats.to_keyset = cte__from_stats.to_keyset
+  WHERE
+    1 = 1 AND is_safe
 )
-
-SELECT DISTINCT
-    ss.from_frame,
-    ss.from_concept,
-    ss.from_keyset,
-    ss.from_hook_name,
-    e.from_hook,
-    ss.to_frame,
-    ss.to_concept,
-    ss.to_keyset,
-    ss.to_hook_name,
-    e.to_hook
-FROM cte__safe_sigs AS ss
-INNER JOIN cte__edges AS e
-  ON  e.frame          = ss.from_frame
-  AND e.from_concept   = ss.from_concept
-  AND e.from_keyset    = ss.from_keyset
-  AND e.from_hook_name = ss.from_hook_name
-  AND e.to_concept     = ss.to_concept
-  AND e.to_keyset      = ss.to_keyset;
+SELECT
+  cte__safe_sigs.from_frame,
+  cte__safe_sigs.from_concept,
+  cte__safe_sigs.from_keyset,
+  cte__safe_sigs.from_hook_name,
+  cte__edges.from_hook,
+  cte__safe_sigs.to_frame,
+  cte__safe_sigs.to_concept,
+  cte__safe_sigs.to_keyset,
+  cte__safe_sigs.to_hook_name,
+  cte__edges.to_hook
+FROM cte__safe_sigs
+INNER JOIN cte__edges
+  ON cte__safe_sigs.from_frame = cte__edges.frame
+  AND cte__safe_sigs.from_concept = cte__edges.from_concept
+  AND cte__safe_sigs.from_keyset = cte__edges.from_keyset
+  AND cte__safe_sigs.from_hook_name = cte__edges.from_hook_name
+  AND cte__safe_sigs.to_concept = cte__edges.to_concept
+  AND cte__safe_sigs.to_keyset = cte__edges.to_keyset
